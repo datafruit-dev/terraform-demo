ff05e982d43d12a7ff9226802e54f9ad6c1f62b6
abhichennupati <achennupati03@berkeley.edu>
2025-08-09 18:43:26 -0700

    added backend on s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "terraform-state-bucket-datafruit-demo"
+    key     = "terraform-demo/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
