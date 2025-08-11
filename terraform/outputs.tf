e0395d912651ff551e731f4c6ef7c6768771eadc
abhi <achennupati03@berkeley.edu>
2025-08-10 18:06:22 -0700

    Update terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -40,3 +40,14 @@ output "public_subnet_ids" {
   description = "IDs of the public subnets"
   value       = aws_subnet.public[*].id
 }
+
+# ECR Repository URLs
+output "ecr_backend_repository_url" {
+  description = "URL of the backend ECR repository"
+  value       = aws_ecr_repository.backend.repository_url
+}
+
+output "ecr_frontend_repository_url" {
+  description = "URL of the frontend ECR repository"
+  value       = aws_ecr_repository.frontend.repository_url
+}
