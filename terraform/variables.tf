0d73ba1fe418dbfd8cb013f80b508e9f3e24c764
abhi <achennupati03@berkeley.edu>
2025-08-10 18:51:16 -0700

    Update terraform/variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+# =============================================================================
+# VARIABLES
+# =============================================================================
+
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "application_name" {
+  description = "Name of the application"
+  type        = string
+  default     = "image-editor"
+}
+
+variable "ecr_image_count" {
+  description = "Number of Docker images to keep in ECR repositories"
+  type        = number
+  default     = 10
+}
+
+variable "enable_ecr_scanning" {
+  description = "Enable vulnerability scanning for images pushed to ECR"
+  type        = bool
+  default     = true
+}
